9a566f03fb7bc786e47ee6c91469e9943b702c81
AgustinBadi <absalinas@uc.cl>
2024-01-14 00:03:52 -0300

    Add Main.hs

---
+++ app/Main.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+
+module Main where
+
+import Mastermind (zkValidator)
+import Types (GameDatum, GameRedeemer)
+import PlutusTx (compile, CompiledCode, toBuiltinData)
+import PlutusTx.Prelude (Bool,(.),($))
+import PlutusLedgerApi.V2 (serialiseCompiledCode, ScriptContext)
+import Data.ByteString qualified as B
+import Data.ByteString.Base16 qualified as Base16
+import Data.ByteString.Short qualified as B
+import Prelude qualified
+
+
+compiledZkValidator :: CompiledCode (GameDatum -> GameRedeemer -> ScriptContext -> Bool)
+compiledZkValidator = $$(compile [|| zkValidator ||])
+
+
+main :: Prelude.IO ()
+main = B.writeFile "./output/mastermind.uplc" . Base16.encode $ B.fromShort serialisedScript
+  where
+    script = compiledZkValidator 
+    serialisedScript = serialiseCompiledCode script
+
+
+
